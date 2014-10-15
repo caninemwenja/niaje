08c62eb30e6793a8a12ccdbaf0799a0c8cd74dc0
caninemwenja <mwenja07@gmail.com>
2014-10-16 02:16:03 +0300

    Play around with threads
    they blow up pretty badly, need to rethink this

--- chat2.py
+++ chat2.py
@@ -1,5 +1,6 @@
 import curses
 import zmq
+import threading
 
 prompt_string = ">> "
 start_row = 2
@@ -12,6 +13,8 @@ receiver = context.socket(zmq.SUB)
 sender.connect('tcp://localhost:5556')
 receiver.connect('tcp://localhost:5557')
 receiver.setsockopt(zmq.SUBSCRIBE, b"")
+
+
 
 screen = curses.initscr()
 screen.clear()
@@ -27,43 +30,57 @@ screen.addstr(curses.LINES-1, 0, prompt_string)
 
 screen.noutrefresh()
 messages_win.noutrefresh()
-
 curses.doupdate()
 
 screen.move(curses.LINES-1, len(prompt_string))
 
-while True:
-    try:
-        inp = screen.getstr()
+lock = threading.RLock()
+running = True
 
-        if current_row == curses.LINES-3 or inp == "clear":
-            empty_line = ""
-            for i in range(0, curses.COLS-10):
-                empty_line += " "
-            for row in range(2, curses.LINES-4):
-                actual_win.addstr(row, 2, empty_line)
+def listen_to_updates():
+    global current_row
+    while running:
+        message = receiver.recv_string()
+        with lock:
+            actual_win.addstr(current_row, 2, "msg: "+message)
             actual_win.refresh()
-            current_row = start_row
+            current_row += 1
             screen.move(curses.LINES-1, len(prompt_string))
             screen.clrtoeol()
             screen.move(curses.LINES-1, len(prompt_string))
-            continue
+
+t = threading.Thread(target=listen_to_updates)
+t.start()
+
+while True:
+    try:
+        with lock:
+            inp = screen.getstr()
+
+        if current_row == curses.LINES-3 or inp == "clear":
+            with lock:
+                empty_line = ""
+                for i in range(0, curses.COLS-10):
+                    empty_line += " "
+                for row in range(2, curses.LINES-4):
+                    actual_win.addstr(row, 2, empty_line)
+                actual_win.refresh()
+                current_row = start_row
+                screen.move(curses.LINES-1, len(prompt_string))
+                screen.clrtoeol()
+                screen.move(curses.LINES-1, len(prompt_string))
+                continue
 
         if inp == "quit" or inp == "exit":
             break
 
-        actual_win.addstr(current_row, 2, "me: "+inp)
-        actual_win.refresh()
-
-        current_row += 1
-
-        screen.move(curses.LINES-1, len(prompt_string))
-        screen.clrtoeol()
-        screen.move(curses.LINES-1, len(prompt_string))
+        sender.send_string(inp)
     except KeyboardInterrupt:
         break
     except Exception:
+        running = False
         curses.endwin()
         raise
 
+running = False
 curses.endwin()
