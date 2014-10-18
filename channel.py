141548beb741676227a8af1299cce7b933c85860
caninemwenja <mwenja07@gmail.com>
2014-10-18 13:40:01 +0300

    Use a redis backed message cache
    * offer the option of ordering the messages for retry

--- channel.py
+++ channel.py
@@ -2,6 +2,9 @@ import zmq
 import json
 import uuid
 import time
+import redis
+
+from collections import OrderedDict
 
 
 class Channel(object):
@@ -79,6 +82,30 @@ class JsonChannel(Channel):
 
 class MessageCache(object):
 
+    def get_unconfirmed_messages(self):
+        raise NotImplementedError()
+
+    def get_received_syn_messages(self):
+        raise NotImplementedError()
+
+    def store_message_to_send(self, message_id, message):
+        raise NotImplementedError()
+
+    def confirm(self, message_id):
+        raise NotImplementedError()
+
+    def is_unconfirmed(self, message_id):
+        raise NotImplementedError()
+
+    def is_already_received(self, message_id):
+        raise NotImplementedError()
+
+    def mark_as_received(self, message_id, message):
+        raise NotImplementedError()
+
+
+class MemoryMessageCache(MessageCache):
+
     def __init__(self):
         self.sent = {}
         self.received = {}
@@ -107,15 +134,95 @@ class MessageCache(object):
         self.received[message_id] = message
 
 
+class OrderedMessageCache(MemoryMessageCache):
+
+    def __init__(self):
+        self.sent = OrderedDict()
+        self.received = OrderedDict()
+
+
+class RedisMessageCache(MessageCache):
+
+    def __init__(self, sent_name, received_name):
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self.sent_name = sent_name
+        self.received_name = received_name
+
+    def serialize(self, data):
+        return json.dumps(data)
+
+    def unserialize(self, data):
+        return json.loads(data)
+
+    def get_unconfirmed_messages(self):
+        messages = [self.unserialize(message) for message in self.r.hvals(self.sent_name)]
+        return [message for message in messages if message['status'] == 'SYN']
+
+    def get_received_syn_messages(self):
+        messages = [self.unserialize(message) for message in self.r.hvals(self.received_name)]
+        return [message for message in messages if message['status'] == 'SYN']
+
+    def store_message_to_send(self, message_id, message):
+        self.r.hset(self.sent_name, message_id, self.serialize(message))
+
+    def confirm(self, message_id):
+        message = self.unserialize(self.r.hget(self.sent_name, message_id))
+        message['status'] = 'ACK'
+        self.r.hset(self.sent_name, message_id, self.serialize(message))
+
+    def is_unconfirmed(self, message_id):
+        return self.r.hexists(self.sent_name, message_id) and self.unserialize(self.r.hget(self.sent_name, message_id))['status'] == 'SYN'
+
+    def is_already_received(self, message_id):
+        return self.r.hexists(self.received_name, message_id)
+
+    def mark_as_received(self, message_id, message):
+        self.r.hset(self.received_name, message_id, self.serialize(message))
+
+
+class OrderedRedisMessageCache(RedisMessageCache):
+
+    def get_unconfirmed_messages(self):
+        key_list = self.sent_name + "_keys"
+        message_ids = self.r.lrange(key_list, 0, self.r.llen(key_list))
+
+        if not message_ids:
+            return []
+
+        messages = [self.unserialize(message) for message in self.r.hmget(self.sent_name, *message_ids)]
+        return [message for message in messages if message['status'] == 'SYN']
+
+    def get_received_syn_messages(self):
+        key_list = self.received_name + "_keys"
+        message_ids = self.r.lrange(key_list, 0, self.r.llen(key_list))
+
+        if not message_ids:
+            return []
+
+        messages = [self.unserialize(message) for message in self.r.hmget(self.received_name, *message_ids)]
+        return [message for message in messages if message['status'] == 'SYN']
+
+    def store_message_to_send(self, message_id, message):
+        key_list = self.sent_name + "_keys"
+        self.r.rpush(key_list, message_id)
+
+        super(OrderedRedisMessageCache, self).store_message_to_send(message_id, message)
+
+    def mark_as_received(self, message_id, message):
+        key_list = self.received_name + "_keys"
+        self.r.rpush(key_list, message_id)
+
+        super(OrderedRedisMessageCache, self).mark_as_received(message_id, message)
+
+
 class ReliableChannel(JsonChannel):
 
     def __init__(self, *args, **kwargs):
         super(ReliableChannel, self).__init__(*args, **kwargs)
 
         self.current_message_id = self.generate_new_message_id()
-        self.message_cache = MessageCache()
-        # self.received = {}
-        # self.sent = {}
+        self.message_cache = OrderedRedisMessageCache(self.identity + "_sent",
+                                                      self.identity + "_received")
 
     def get_current_id(self):
         return self.identity + ":::" + str(self.current_message_id)
